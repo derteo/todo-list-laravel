d75cb2a6799404367588b14a3b34929df2f23315
mxtte <mmatteoderosa@gmail.com>
2026-05-06 16:20:03 +0200

    added migrations
    added default laravel api migration and the two tables' migrations

---
+++ app/database/migrations/2026_05_06_134606_create_checklists_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('checklists', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->integer("paranoid");
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('checklists');
+    }
+};
