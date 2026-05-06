68b47b71f9e17ebb0abedaa53ceb624da3d2b040
mxtte <mmatteoderosa@gmail.com>
2026-05-06 16:21:27 +0200

    added routes
    added default api resources for both the tables, will add more detailed routes later

---
+++ app/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ChecklistController;
+use App\Http\Controllers\NoteController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource("checklists", ChecklistController::class);
+Route::apiResource("notes", NoteController::class);
